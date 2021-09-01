7b586a4eea9d9c7762940c0f88a56ca18f1dcf3f
cholestrol <matt725.md@gmail.com>
2021-09-01 10:47:24 -0600

    Add gold / platinum to crafting recipe

--- Items/Small/_BaseSmallCharm.cs
+++ Items/Small/_BaseSmallCharm.cs
@@ -11,16 +11,30 @@ namespace BuffCharm.Items.Small
         public override int Rarity => ItemRarityID.Green;
         public override void AddRecipes()
         {
-            for (int i = 0; i < PotionsCraftedFrom.GetLength(0); i++)
+            for (int i = 0; i < 2; i++)
             {
-                ModRecipe recipe = new ModRecipe(mod);
-                for (int j = 0; j < PotionsCraftedFrom.GetLength(1); j++)
+                for (int j = 0; j < PotionsCraftedFrom.GetLength(0); j++)
                 {
-                    recipe.AddIngredient(PotionsCraftedFrom[i, j], PotionAmountRequired);
+                    ModRecipe recipe = new ModRecipe(mod);
+                    for (int k = 0; k < PotionsCraftedFrom.GetLength(1); k++)
+                    {
+                        recipe.AddIngredient(PotionsCraftedFrom[j, k], PotionAmountRequired);
+                    }
+                    switch (i)
+                    {
+                        case (0):
+                            recipe.AddIngredient(ItemID.GoldBar, 6);
+                            break;
+                        case (1):
+                            recipe.AddIngredient(ItemID.PlatinumBar, 6);
+                            break;
+                        default:
+                            break;
+                    }
+                    recipe.AddTile(TileID.Anvils);
+                    recipe.SetResult(this);
+                    recipe.AddRecipe();
                 }
-                recipe.AddTile(TileID.Anvils);
-                recipe.SetResult(this);
-                recipe.AddRecipe();
             }
         }
     }
